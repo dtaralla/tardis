9f136c623977be69c50db4c04e4ffea8cf13efc1
Detlev Casanova <detlev.casanova@gmail.com>
2021-12-07 17:48:13 -0500

    WIP: Use modified spice crate to load NAIF files

--- examples/tle.rs
+++ examples/tle.rs
@@ -8,6 +8,8 @@ use tardis::sun::Sun;
 use tardis::tle::TLE;
 use tardis::traits::Observable;
 
+use spice;
+
 
 fn main() -> Result<(), String> {
     let tle_lines = vec![
@@ -15,6 +17,12 @@ fn main() -> Result<(), String> {
         "1 25544U 98067A   21316.58314353 -.00007551  00000-0 -13101-3 0  9994".as_bytes(),
         "2 25544  51.6442 328.9484 0004731 186.1225 318.0089 15.48559922311590".as_bytes(),
     ];
+
+    spice::furnsh(String::from("/home/detlev/Downloads/de405.bsp").as_str());
+    //let et = spice::str2et("2021-DEC-12 16:00:00");
+    //println!("{}", et);
+    let (position, light_time) = spice::spkpos("MARS", 0.0, "J2000", "NONE", "SUN");
+    println!("{:?} {:?}", position, light_time);
 
     /*
     let mont_royal_coordinates = Coordinates::new(
